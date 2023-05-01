46bd89aaa0e039ca836cb1528568b21119280320
dinarfathrahmanov <dinfat@mail.ru>
2023-05-01 20:12:21 +0300

    first commit

---
+++ src/test/java/com/listemployee/listemployee/ListEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.listemployee.listemployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListEmployeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
