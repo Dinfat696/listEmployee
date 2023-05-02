ba9a97b2a795deead1a23a42d4720730e1460cda
dinarfathrahmanov <dinfat@mail.ru>
2023-05-02 20:43:27 +0300

    готово

---
+++ src/main/java/com/listemployee/listemployee/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.listemployee.listemployee;
+
+public interface EmployeeService {
+    Employee addEmployee(String firstName, String lastName);
+    Employee removeEmployee(String firstName, String lastName);
+    Employee findEmployee(String firstName, String lastName);
+}
